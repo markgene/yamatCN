f62697196982ab99816d49c45ebbb7e3627e2576
Mark J Chen <mjchen.gene@gmail.com>
2018-11-12 12:42:45 -0600

    feat: add private functions to get reference and query sample names.

--- R/conumee-pipe.R
+++ R/conumee-pipe.R
@@ -223,3 +223,41 @@ cn_pipe_conumee <-
   }
   cnv_dat
 }
+#' Query sample names.
+#'
+#' @param x An object of \code{\link[minfi]{RGChannelSet-class}} or
+#'   \code{\link[minfi]{MethylSet-class}} or
+#'   \code{\link[minfi]{GenomicMethylSet-class}}.
+#' @param label A character vector labeling query samples
+#' @return A character vector of query sample names.
+#' @noRd
+.query_sample_names <- function(x, label = "query") {
+  query_sample_names <-
+    minfi::pData(x) %>%
+    as.data.frame() %>%
+    dplyr::filter(ref_query %in% label) %>%
+    dplyr::select(Sample_Name) %>%
+    unlist()
+  names(query_sample_names) <- query_sample_names
+  query_sample_names
+}
+
+
+#' Reference sample names.
+#'
+#' @param x An object of \code{\link[minfi]{RGChannelSet-class}} or
+#'   \code{\link[minfi]{MethylSet-class}} or
+#'   \code{\link[minfi]{GenomicMethylSet-class}}.
+#' @param label A character vector labeling reference samples
+#' @return A character vector of reference sample names.
+#' @noRd
+.reference_sample_names <- function(x, label = "ref") {
+  ref_sample_names <-
+    minfi::pData(x) %>%
+    as.data.frame() %>%
+    dplyr::filter(ref_query %in% label) %>%
+    dplyr::select(Sample_Name) %>%
+    unlist()
+  names(ref_sample_names) <- ref_sample_names
+  ref_sample_names
+}
