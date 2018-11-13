8198ef64f693e9834ca3890ae17a28a5dd8e5a77
Mark J Chen <mjchen.gene@gmail.com>
2018-11-13 10:50:49 -0600

    feat: add dnacopy_analysis functions.
    * dnacopy_analysis(): general purpose.
    * dnacopy_analysis.default(): dnacopy_analysis with default settings.
    * dnacopy_analysis.conumee(): dnacopy_analysis with Conumee settings.
    * dnacopy_analysis.yamat(): dnacopy_analysis with yamat settings.

--- R/DNAcopy.R
+++ R/DNAcopy.R
@@ -1,2 +1,168 @@
 # Wrapper around DNAcopy
 
+
+#' Run DNAcopy analysis.
+#'
+#' @param x A \code{minfi} object such as
+#'   \code{\link[minfi]{RGChannelSet-class}} or
+#'   \code{\link[minfi]{MethylSet-class}} or
+#'   \code{\link[minfi]{GenomicMethylSet-class}} or
+#'   \code{\link[minfi]{GenomicRatioSet-class}}.
+#' @param lrr A matrix of log2 transformed ratio of query and reference copy
+#'   number (sum of methylation and unmethylation signals).
+#' @param seed An integer scalar to set the seed. Default to 1.
+#' @param verbose A logical scalar. Default to TRUE.
+#' @param ... Arugments passed to \code{\link[DNAcopy]{segment}}.
+#' @return A \code{\link[DNAcopy]{DNAcopy}} object.
+#' @details The function provides a template of run DNAcopy analysis. There are
+#'   many parameters in smoothing and segmentation. The function uses the
+#'   default settings for smoothing.
+#' @export
+dnacopy_analysis <- function(x, lrr, seed = 1, verbose = TRUE, ...) {
+  # Creating DNAcopy CNA object.
+  if (verbose) {
+    message("Creating DNAcopy CNA object...")
+    tictoc::tic()
+  }
+  cna_obj <- .create_dnacopy_cna(x, lrr)
+  if (verbose) tictoc::toc()
+  # Smooth
+  if (verbose) {
+    message("Smoothing...")
+    tictoc::tic()
+  }
+  cna_obj <-
+    DNAcopy::smooth.CNA(
+      cna_obj,
+      smooth.region = 10,
+      outlier.SD.scale = 4,
+      smooth.SD.scale = 2,
+      trim = 0.025
+    )
+  if (verbose) tictoc::toc()
+  # Segmentation
+  if (verbose) {
+    message("Segmentation...")
+    tictoc::tic()
+  }
+  dnacopy_obj <- DNAcopy::segment(cna_obj, verbose = verbose, ...)
+  if (verbose) tictoc::toc()
+  dnacopy_obj
+}
+
+
+#' Run DNAcopy analysis: DNAcopy's default settings.
+#'
+#' Use the default settings of \code{\link[DNAcopy]{segment}}.
+#'
+#' @param x A \code{minfi} object such as
+#'   \code{\link[minfi]{RGChannelSet-class}} or
+#'   \code{\link[minfi]{MethylSet-class}} or
+#'   \code{\link[minfi]{GenomicMethylSet-class}} or
+#'   \code{\link[minfi]{GenomicRatioSet-class}}.
+#' @param lrr A matrix of log2 transformed ratio of query and reference copy
+#'   number (sum of methylation and unmethylation signals).
+#' @param seed An integer scalar to set the seed. Default to 1.
+#' @param verbose A logical scalar. Default to TRUE.
+#' @return A \code{\link[DNAcopy]{DNAcopy}} object.
+#' @export
+dnacopy_analysis.default <- function(x, lrr, seed = 1, verbose = TRUE) {
+  dnacopy_analysis(
+    x,
+    lrr,
+    seed = seed,
+    verbose = verbose,
+    weights = NULL,
+    alpha = 0.01,
+    nperm = 10000,
+    p.method = "hybrid",
+    min.width = 2,
+    kmax = 25,
+    nmin = 200,
+    eta = 0.05,
+    sbdry = NULL,
+    trim = 0.025,
+    undo.splits = "none",
+    undo.prune = 0.05,
+    undo.SD = 3
+  )
+}
+
+
+#' Run DNAcopy analysis: Conumee's settings.
+#'
+#' Use Conumee's settings of \code{\link[DNAcopy]{segment}}. Notice: Conumee
+#' uses the bins of probes as markers rather than probes themselves.
+#'
+#' @param x A \code{minfi} object such as
+#'   \code{\link[minfi]{RGChannelSet-class}} or
+#'   \code{\link[minfi]{MethylSet-class}} or
+#'   \code{\link[minfi]{GenomicMethylSet-class}} or
+#'   \code{\link[minfi]{GenomicRatioSet-class}}.
+#' @param lrr A matrix of log2 transformed ratio of query and reference copy
+#'   number (sum of methylation and unmethylation signals).
+#' @param seed An integer scalar to set the seed. Default to 1.
+#' @param verbose A logical scalar. Default to TRUE.
+#' @return A \code{\link[DNAcopy]{DNAcopy}} object.
+#' @export
+dnacopy_analysis.conumee <- function(x, lrr, seed = 1, verbose = TRUE) {
+  dnacopy_analysis(
+    x,
+    lrr,
+    seed = seed,
+    verbose = verbose,
+    weights = NULL,
+    alpha = 0.001,
+    nperm = 50000,
+    p.method = "hybrid",
+    min.width = 5,
+    kmax = 25,
+    nmin = 200,
+    eta = 0.05,
+    sbdry = NULL,
+    trim = 0.025,
+    undo.splits = "sdundo",
+    undo.prune = 0.05,
+    undo.SD = 2.2,
+    verbose = verbose
+  )
+}
+
+
+#' Run DNAcopy analysis: Yamat's settings.
+#'
+#' Use Yamat's settings of \code{\link[DNAcopy]{segment}}.
+#'
+#' @param x A \code{minfi} object such as
+#'   \code{\link[minfi]{RGChannelSet-class}} or
+#'   \code{\link[minfi]{MethylSet-class}} or
+#'   \code{\link[minfi]{GenomicMethylSet-class}} or
+#'   \code{\link[minfi]{GenomicRatioSet-class}}.
+#' @param lrr A matrix of log2 transformed ratio of query and reference copy
+#'   number (sum of methylation and unmethylation signals).
+#' @param seed An integer scalar to set the seed. Default to 1.
+#' @param verbose A logical scalar. Default to TRUE.
+#' @return A \code{\link[DNAcopy]{DNAcopy}} object.
+#' @export
+dnacopy_analysis.yamat <- function(x, lrr, seed = 1, verbose = TRUE) {
+  dnacopy_analysis(
+    x,
+    lrr,
+    seed = seed,
+    verbose = verbose,
+    weights = NULL,
+    alpha = 0.001,
+    nperm = 50000,
+    p.method = "hybrid",
+    min.width = 5,
+    kmax = 25,
+    nmin = 200,
+    eta = 0.05,
+    sbdry = NULL,
+    trim = 0.025,
+    undo.splits = "sdundo",
+    undo.prune = 0.05,
+    undo.SD = 3,
+    verbose = verbose
+  )
+}
