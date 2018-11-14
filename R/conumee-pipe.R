72cc433d3af9fb8e7052a5f9a7fc386936ac47ee
Mark J Chen <mjchen.gene@gmail.com>
2018-11-14 08:44:49 -0600

    feat: add conumee_backbone() deals with Conumee pipeline steps after preprocessing.

--- R/conumee-pipe.R
+++ R/conumee-pipe.R
@@ -154,3 +154,83 @@ conumee_analysis <- function(query, ref, anno, intercept = TRUE, seed = 1, ...) 
   conumee::CNV.segment(cnv_anl, ...)
 }
 
+
+#' CNV pipeline backbone.
+#'
+#' CNV pipeline backbone is the steps after preprocessing, including loading
+#' data into Conumee, analysis and write the result.
+#'
+#' @param x An object of \code{\link[minfi]{GenomicRatioSet-class}} or
+#'   \code{\link[minfi]{GenomicMethylSet-class}}.
+#' @param report_dir A character scalar of reporting.
+#' @param pipe_file A character scalar of the filename storing the pipeline
+#'   result sample by sample. Default to "conumee.Rda". Turn off the saving by
+#'   set it to NULL.
+#' @param dnacopy_seed An integer scalar to set the seed. Default to 1.
+#' @param overwrite A logical scalar. Default to FALSE.
+#' @param verbose A logical scalar. Default to TRUE.
+#' @param ... Any arguments passed to \code{\link[conumee]{CNV.segment}}.
+#' @return An object of \code{\link{ConumeePipe}} class.
+#' @details The \code{ref} and \code{qry} should include at least the following
+#'   columns \code{Sample_Name}, \code{Gender}, \code{Batch}, \code{Sample_Prep}.
+#'   Batch effects are removed on methylation and unmethylation signals (log2
+#'   transformed) separately.
+#' @export
+conumee_backbone <- function(x,
+                             report_dir,
+                             pipe_file = "conumee.Rda",
+                             dnacopy_seed = 1,
+                             overwrite = FALSE,
+                             verbose = TRUE,
+                             ...) {
+  # Run Conumee-based CNV analysis
+  # Create Conumee annotation.
+  if (verbose) {
+    message("Preparing CNV analysis: annotation...")
+    tictoc::tic()
+  }
+  cnv_anno <- CNV.create_anno.yamat(x, detail_regions_preset = "conumee")
+  if (verbose) tictoc::toc()
+  # Load data to Conumee.
+  if (verbose) {
+    message("Preparing CNV analysis: loading data...")
+    tictoc::tic()
+  }
+  cnv_dat <- .CNV_load(x)
+  if (verbose) tictoc::toc()
+  # Main Conumee CNV analysis.
+  if (verbose) {
+    message("Running CNV analysis: main analysis...")
+    tictoc::tic()
+  }
+  ref_samples <- .reference_sample_names(x, label = "ref")
+  qry_samples <- .query_sample_names(x, label = "query")
+  cnv_results <- lapply(
+    qry_samples,
+    function(nm) {
+      i <- which(qry_samples == nm)
+      if (verbose) {
+        message("Run CNV for sample ", "(", i, "/", length(qry_samples), "): ", nm)
+        tictoc::tic()
+      }
+      cnv_res <-
+        conumee_analysis(cnv_dat[nm], cnv_dat[ref_samples], anno = cnv_anno, seed = dnacopy_seed, ...)
+      if (verbose) tictoc::toc()
+      cnv_res
+    })
+  if (verbose) tictoc::toc()
+  # Create ConumeePipe object
+  dat <- new.env(parent = parent.frame())
+  dat$conumee_results <- cnv_results
+  dat$minfi_obj <- x
+  cp_obj <- ConumeePipe(dat = dat)
+  # Save the result
+  if (is.null(pipe_file)) {
+    if (verbose)
+      message("Skip saving the pipeline result sample by sample.")
+  } else {
+    save_pipe(cp_obj, outdir = report_dir, filename = pipe_file)
+  }
+  cp_obj
+}
+
