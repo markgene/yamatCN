2859060a3442dca5c257908c47b2c25222e1ecc1
Mark J Chen <mjchen.gene@gmail.com>
2018-11-14 11:05:35 -0600

    feat: add private function to write conumee result.

--- R/write-segments.R
+++ R/write-segments.R
@@ -37,4 +37,43 @@ setMethod(
 )
 
 
+#' Output Conumee analysis result as table of one sample
+#'
+#' @param cnv_anl A \code{CNV.analysis} object (single sample)
+#' @param outdir A character scalar of output directory
+#' @param what A character scalar. This should be (an unambiguous
+#' abbreviation of) one of 'probes', 'bins', 'detail' or 'segments'.
+#' Defaults to 'segments'.
+#' @param outfile A character scalar of output file name.
+#' @param prefix A character scalar of output file prefix. Default is
+#' sample name. The output file name is the prefix + "-" + \code{what}
+#' argument + ".csv". For example, if sample name is "MA001" and
+#' \code{what} is "probes", the output file name will be "MA001-probes.csv".
+#' @param overwrite A logical scalar. Default to FALSE.
+#' @return A character scalar of output file path.
+#' @noRd
+.write_conumee_result <-
+  function(cnv_anl,
+           outdir,
+           what = "segments",
+           prefix = NULL,
+           outfile = NULL,
+           overwrite = FALSE) {
+    if (is.null(outfile)) {
+    if (is.null(prefix)) {
+      outfile <- paste0(names(cnv_anl), "-", what, ".csv")
+    } else {
+      outfile <- paste0(prefix, what, ".csv")
+    }
+  }
+  out_fp <- file.path(outdir, outfile)
+  if (file.exists(out_fp) & !overwrite) {
+    message("Output file ", out_fp,  " has existed. Skip writing.")
+  } else {
+    df <- conumee::CNV.write(cnv_anl, what = what)
+    write.csv(df, file = out_fp, row.names = FALSE, quote = FALSE)
+  }
+  out_fp
+}
+
 
