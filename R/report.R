7a05d26c590395b67154943cf593e05357210171
Mark J Chen <mjchen.gene@gmail.com>
2018-11-21 10:29:23 -0600

    feat: add functions to convert between sample IDs/names used in DNAcopy and minfi.
    If a sample ID begins with a digital number, the DNAcopy package will add "X" to the beginning.

--- R/report.R
+++ R/report.R
@@ -34,3 +34,45 @@ setMethod(
   }
 )
 
+
+#' To \code{minfi} sample name.
+#'
+#' The function translates sample IDs of \code{DNAcopy} to those of the input
+#' \code{minfi} object. If a sample ID begins with a digital number, the
+#' \code{DNAcopy} package will add "X" to the beginning.
+#'
+#' @param x An object of \code{\link{CwobPipe}} class.
+#' @param sample_id A character scalar of sample ID used in \code{DNAcopy} result.
+#' @return A character scalar of sample name used in \code{minfi} object.
+#' @noRd
+.to_minfi_sample_name <- function(x, sample_id) {
+  sample_ids <- minfi::sampleNames(x@dat$minfi)
+  if (!sample_id %in% sample_ids) {
+    x_sample_ids <- paste0("X", sample_ids)
+    if (sample_id %in% x_sample_ids) {
+      sample_id <- sample_ids[sample_id == x_sample_ids]
+    } else {
+      stop("Fail to find sample ID in DNAcopy result in minfi object.")
+    }
+  }
+  sample_id
+}
+
+
+#' To \code{DNAcopy} sample IDs.
+#'
+#' The function translates sample IDs used in \code{minfi} object to those
+#' in \code{DNAcopy} result. If a sample ID begins with a digital number, the
+#' \code{DNAcopy} package will add "X" to the beginning.
+#'
+#' @param minfi_obj A minfi object such as an object of
+#'     \code{\link[minfi]{GenomicRatioSet-class}} or
+#'     \code{\link[minfi]{GenomicMethylSet-class}}.
+#' @return A character vector of sample IDs used in \code{DNAcopy}.
+#' @noRd
+.to_DNAcopy_sample_ids <- function(minfi_obj) {
+  sample_ids <- minfi::sampleNames(minfi_obj)
+  ifelse(stringr::str_detect(sample_ids, "^[0-9]"),
+         paste0("X", sample_ids),
+         sample_ids)
+}
